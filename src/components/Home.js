3886ddb339dd8a56ef0fdd1a35102b845a7df706
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2024-12-19 13:38:43 +0200

    init

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to ClimbHub</h1>
+            <p>Your one-stop destination for all things climbing and NIGGGGGGAAAAAA.</p>
+        </div>
+    );
+};
+
+export default Home;
